632ba62f760ea7debe7bfab651cb02f10a71b886
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-08-01 16:19:05 +0545

    push

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
 
 
-const getfacts = async () => {
-    console.log("getting data .....");
-    let response = await fetch(URL);
-    console.log(response);
+//const getFacts = async () => {
+    //console.log("getting data .....");
+    //let response = await fetch(URL);
+    //console.log(response);
+    //let data = await response.json();
+    //factPara.innerText = data[2].text;
+//};
+
+function getFacts(){
+    fetch(URL)
+    .then((response) => {
+       return response.json(); 
+    })
+    .then((data) => {
+        console.log(data);
+        factPara.innerText = data[4].text;
+    });
 }
+
+
+btn.addEventListener("click",getfacts);
